4a9590f0934f200bcbf04b6b03c36049d576c7d8
Fahd-Badi-Abdelhameed <77761857+Fahd-Badi-Abdelhameed@users.noreply.github.com>
2022-04-10 08:01:16 +0200

    Update LCD_program.c

--- 03- HAL/03- LCD/V1/LCD_program.c
+++ 03- HAL/03- LCD/V1/LCD_program.c
@@ -16,8 +16,7 @@
 /******************************************************************************
 ********************************* Delay DIRECTIVES ****************************
 ******************************************************************************/
-#include <avr/delay.h>
-
+#include <util/delay.h>
 
 /******************************************************************************
 ********************************* DIO DIRECTIVES ******************************
@@ -41,102 +40,55 @@
 * Description 	: LCD Display Char on LCD Function.						      *
 ******************************************************************************/
 
-void LCD_voidWriteData(uint8 Copy_u8Data){
-
+void LCD_voidWriteData(uint8 Copy_u8Data)
+{
 	DIO_voidSetPinValue(LCD_RS_PIN, DIO_HIGH);
 	DIO_voidSetPinValue(LCD_RW_PIN, DIO_LOW );
 
-	if(LCD_MODE == MODE_8_BIT){
+	#if (LCD_MODE == MODE_8_BIT)
 		
-		DIO_voidSetPortValue(LCD_PORT_DATA, Copy_u8Data);
+		DIO_voidSetPinValue(LCD_D0_PIN, GET_BIT(Copy_u8Data, 0));
+		DIO_voidSetPinValue(LCD_D1_PIN, GET_BIT(Copy_u8Data, 1));
+		DIO_voidSetPinValue(LCD_D2_PIN, GET_BIT(Copy_u8Data, 2));
+		DIO_voidSetPinValue(LCD_D3_PIN, GET_BIT(Copy_u8Data, 3));
+		DIO_voidSetPinValue(LCD_D4_PIN, GET_BIT(Copy_u8Data, 4));
+		DIO_voidSetPinValue(LCD_D5_PIN, GET_BIT(Copy_u8Data, 5));
+		DIO_voidSetPinValue(LCD_D6_PIN, GET_BIT(Copy_u8Data, 6));
+		DIO_voidSetPinValue(LCD_D7_PIN, GET_BIT(Copy_u8Data, 7));
 		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_HIGH);
-		_delay_ms(1);
-		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_LOW);
-		_delay_ms(1);
-		
-	}
-	
-	else if(LCD_MODE == MODE_4_BIT){
+		LCD_voidLatchEnable();
 
-		DIO_voidSetPortValue(LCD_PORT_DATA, Copy_u8Data & 0xF0);
+	#elif (LCD_MODE == MODE_4_BIT)
+
+		DIO_voidSetPinValue(LCD_D4_PIN, GET_BIT(Copy_u8Data, 4));
+		DIO_voidSetPinValue(LCD_D5_PIN, GET_BIT(Copy_u8Data, 5));
+		DIO_voidSetPinValue(LCD_D6_PIN, GET_BIT(Copy_u8Data, 6));
+		DIO_voidSetPinValue(LCD_D7_PIN, GET_BIT(Copy_u8Data, 7));
 		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_HIGH);
-		_delay_ms(1);
+		LCD_voidLatchEnable();
+
+		DIO_voidSetPinValue(LCD_D4_PIN, GET_BIT(Copy_u8Data, 0));
+		DIO_voidSetPinValue(LCD_D5_PIN, GET_BIT(Copy_u8Data, 1));
+		DIO_voidSetPinValue(LCD_D6_PIN, GET_BIT(Copy_u8Data, 2));
+		DIO_voidSetPinValue(LCD_D7_PIN, GET_BIT(Copy_u8Data, 3));
 		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_LOW);
-		_delay_ms(1);
-		
-		DIO_voidSetPortValue(LCD_PORT_DATA, Copy_u8Data << 4);
-		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_HIGH);
-		_delay_ms(1);
-		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_LOW);
-		_delay_ms(1);
-		
-	}	
+		LCD_voidLatchEnable();
 	
-	else{  /* return error */  }
-		
+	#endif
+	
 }
-
-
-/******************************************************************************
-* Description 	: LCD Send Command Function.						          *
-******************************************************************************/
-
-void LCD_voidSendCommand(uint8 Copy_u8Command){
-
-	DIO_voidSetPinValue (LCD_RS_PIN, DIO_LOW);
-	DIO_voidSetPinValue (LCD_RW_PIN, DIO_LOW);
-	
-	if(LCD_MODE == MODE_8_BIT){
-		
-		DIO_voidSetPortValue(LCD_PORT_DATA, Copy_u8Command);
-		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_HIGH);
-		_delay_ms(1);
-		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_LOW);
-		_delay_ms(1);
-		
-	}
-	
-	else if(LCD_MODE == MODE_4_BIT){
-
-		DIO_voidSetPortValue(LCD_PORT_DATA, Copy_u8Command & 0xF0);
-		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_HIGH);
-		_delay_ms(1);
-		
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_LOW);
-		_delay_ms(1);
-	
-		DIO_voidSetPortValue(LCD_PORT_DATA, Copy_u8Command << 4);
-	
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_HIGH);
-		_delay_ms(1);
-	
-		DIO_voidSetPinValue(LCD_EN_PIN, DIO_LOW);
-		_delay_ms(1);
-		
-	}	
-	
-	else{  /* return error */  }
-
-}
-
 
 /******************************************************************************
 * Description 	: LCD Initialization Function.						          *
 ******************************************************************************/
 
-void LCD_voidInit(void){
-
-	/* set port data as output */
-	DIO_voidSetPortDirection(LCD_PORT_DATA, 0xFF);
+void LCD_voidInit(void)
+{
+	/* set lcd pins data as output */
+	DIO_voidSetPinDirection(LCD_D4_PIN, DIO_OUTPUT);
+	DIO_voidSetPinDirection(LCD_D5_PIN, DIO_OUTPUT);
+	DIO_voidSetPinDirection(LCD_D6_PIN, DIO_OUTPUT);
+	DIO_voidSetPinDirection(LCD_D7_PIN, DIO_OUTPUT);
 		
 	/* set RS, RW, EN pins  as output */
 	DIO_voidSetPinDirection(LCD_RS_PIN, DIO_OUTPUT);
@@ -144,25 +96,27 @@ void LCD_voidInit(void){
 	DIO_voidSetPinDirection(LCD_EN_PIN, DIO_OUTPUT);
 		
 	/* Init data port & EN by zero */
-	DIO_voidSetPortValue(LCD_PORT_DATA, DIO_LOW);
 	DIO_voidSetPinValue(LCD_EN_PIN, DIO_LOW);
 	
 	/* Delay 30ms to ensure the initialization of the LCD driver */
     _delay_ms(30);
 
-	
-	#if LCD_MODE == MODE_8_BIT
+	#if (LCD_MODE == MODE_8_BIT)
 
-		LCD_voidSendCommand(FUNCTION_SET);
+		DIO_voidSetPinDirection(LCD_D0_PIN, DIO_OUTPUT);
+		DIO_voidSetPinDirection(LCD_D1_PIN, DIO_OUTPUT);
+		DIO_voidSetPinDirection(LCD_D2_PIN, DIO_OUTPUT);
+		DIO_voidSetPinDirection(LCD_D3_PIN, DIO_OUTPUT);
+		_delay_ms(30);
+
+		LCD_voidSendCommand(FUNCTION_SET_8BIT);
 		_delay_ms(1);
-
 		LCD_voidSendCommand(DISPLAY_ON_OFF);
 		_delay_ms(1);
-
 		LCD_voidSendCommand(DISPLAY_CLEAR);
 		_delay_ms(2);
 		
-	#elif LCD_MODE == MODE_4_BIT
+	#elif (LCD_MODE == MODE_4_BIT)
 		
 		LCD_voidSendCommand(0x33);
 		LCD_voidSendCommand(0x32);
@@ -171,11 +125,9 @@ void LCD_voidInit(void){
 		
 		LCD_voidSendCommand(DISPLAY_ON_OFF);
 		_delay_ms(1);
-		
 		LCD_voidSendCommand(DISPLAY_CLEAR);
 		_delay_ms(2);
-		
-		LCD_voidSendCommand(0x06);
+		LCD_voidSendCommand(ENTRY_MODE);
 		
 	#else 
 		#erroe("You Chosed Wrong LCD Mode")
@@ -183,63 +135,67 @@ void LCD_voidInit(void){
 }
 
 
-
 /******************************************************************************
 * Description 	: LCD Clear Screen Function.						          *
 ******************************************************************************/
 
-void LCD_voidClearScreen(void){
-
+void LCD_voidClearScreen(void)
+{
 	LCD_voidSendCommand(DISPLAY_CLEAR);
 	_delay_ms(2);
 }
 
-void LCD_voidCursorOff(void){
+/******************************************************************************
+* Description 	: LCD Cursor OFF Function.						              *
+******************************************************************************/
 
+void LCD_voidCursorOff(void)
+{
 	LCD_voidSendCommand(CURSOR_OFF);
+	_delay_ms(2);
 }
 
-void LCD_voidCursorOn(void){
+/******************************************************************************
+* Description 	: LCD Cursor ON Function.						              *
+******************************************************************************/
 
+void LCD_voidCursorOn(void)
+{
 	LCD_voidSendCommand(CURSOR_ON);
+	_delay_ms(2);
 }
 
 /******************************************************************************
 * Description 	: LCD Write String Function.						          *
 ******************************************************************************/
 
-void LCD_voidWriteString(uint8 *Copy_u8Ptr){
-	
+void LCD_voidWriteString(char *Copy_cPtrData)
+{
 	uint8 Local_u8Counter = 0;
 
-	while(Copy_u8Ptr[Local_u8Counter] != 0){
-	
-		LCD_voidWriteData(Copy_u8Ptr[Local_u8Counter]);
+	while(0 != Copy_cPtrData[Local_u8Counter])
+	{
+		LCD_voidWriteData(Copy_cPtrData[Local_u8Counter]);
 		Local_u8Counter++;
-	
 	}
-
 }
-
 
 /******************************************************************************
 * Description 	: LCD Set Cursor Function.						              *
 ******************************************************************************/
 
-void LCD_voidSetPosition(uint8 Copy_u8Row, uint8 Copy_u8Col){
-
-	if(0 == Copy_u8Row){
-
+void LCD_voidSetPosition(uint8 Copy_u8Row, uint8 Copy_u8Col)
+{
+	if(0 == Copy_u8Row)
+	{
 		/* set ddram */
-		LCD_voidSendCommand(128 + Copy_u8Col);
+		LCD_voidSendCommand(LCD_SET_DDRAM_ROW0 + Copy_u8Col);
 	}
-
-	else if(1 == Copy_u8Row){
-
+	else if(1 == Copy_u8Row)
+	{
 		/* set ddram */
-		LCD_voidSendCommand(128 + 64 + Copy_u8Col);
+		LCD_voidSendCommand(LCD_SET_DDRAM_ROW1 + Copy_u8Col);
 	}
-
 	else{  /* return error */ }
 
 }
@@ -249,13 +205,15 @@ void LCD_voidSetPosition(uint8 Copy_u8Row, uint8 Copy_u8Col){
 * Description 	: LCD Display Loop String or Char on lcd Function             *
 ******************************************************************************/
 
-void LCD_voidDisplayLoop(uint8 Copy_u8Row, uint8 Copy_u8Speed, uint8 *Copy_u8Ptr){
+void LCD_voidDisplayLoop(uint8 Copy_u8Row, uint8 Copy_u8Speed, char *Copy_cPtr)
+{
+	uint8 Local_u8Counter = 0;
 
-	for(uint8 i=0; i<15; i++){
+	for(Local_u8Counter=0; Local_u8Counter<15; Local_u8Counter++)
+	{
+		LCD_voidSetPosition(Copy_u8Row, Local_u8Counter);
 
-		LCD_voidSetPosition(Copy_u8Row, i);
-
-		LCD_voidWriteString(Copy_u8Ptr);
+		LCD_voidWriteString(Copy_cPtr);
 		_delay_ms(Copy_u8Speed);
 
 		LCD_voidSendCommand(DISPLAY_CLEAR);
@@ -266,30 +224,29 @@ void LCD_voidDisplayLoop(uint8 Copy_u8Row, uint8 Copy_u8Speed, uint8 *Copy_u8Ptr
 
 
 /******************************************************************************
-* Description 	: LCD Number on lcd Function                                  *
+* Description 	: LCD Write Number on lcd Function.                           *
 ******************************************************************************/
 
-void LCD_voidWriteNumber(uint16 Copy_u16Number){
-
+void LCD_voidWriteNumber(uint16 Copy_u16Number)
+{
 	uint8 Local_u8Counter = 0, Local_u8Digits = 0, Local_u8Current;
 	uint16 Local_u16CopyNumber = Copy_u16Number;
 
-	if(0 == Copy_u16Number){
-
+	if(0 == Copy_u16Number)
+	{
 		LCD_voidWriteData('0');
 	}
-
-	else{
-
-		while(Local_u16CopyNumber){
-
+	else
+	{
+		while(Local_u16CopyNumber)
+		{
 			Local_u16CopyNumber /= 10;
 			Local_u8Digits++;
 		}
 		Local_u16CopyNumber = Copy_u16Number;
 
-		for(Local_u8Counter = 0; Local_u8Counter < Local_u8Digits; Local_u8Counter++){
-
+		for(Local_u8Counter = 0; Local_u8Counter < Local_u8Digits; Local_u8Counter++)
+		{
 			Local_u8Current = Local_u16CopyNumber / (Private_u16GetPower(10, Local_u8Digits - 1 - Local_u8Counter));
 			LCD_voidWriteData(Local_u8Current + '0');
 			Local_u16CopyNumber %= (Private_u16GetPower(10, Local_u8Digits - 1 - Local_u8Counter));
@@ -302,25 +259,25 @@ void LCD_voidWriteNumber(uint16 Copy_u16Number){
 * Description 	: LCD Writr Char (CGRAM) Function                             *
 ******************************************************************************/
 
-void LCD_voidWriteExtraChar(uint8 Copy_u8Row, uint8 Copy_u8Col){
-
+void LCD_voidWriteExtraChar(uint8 Copy_u8Row, uint8 Copy_u8Col, char *Copy_PtrData)
+{
 	uint8 Local_u8Iteration1, Local_u8Iteration2;
 
 	/*DDRAM-->CGRAM*/
-	LCD_voidSendCommand(64);
+	LCD_voidSendCommand(DDRAM_TO_CGRAM);
 
-	for(Local_u8Iteration1 = 0 ; Local_u8Iteration1 < 64; Local_u8Iteration1++){
-
-		LCD_voidWriteData(ExtraChar[Local_u8Iteration1]);
+	for(Local_u8Iteration1 = 0 ; Local_u8Iteration1 < 64; Local_u8Iteration1++)
+	{
+		LCD_voidWriteData(Copy_PtrData[Local_u8Iteration1]);
 	}
 
 	/*CGRAM-->DDRAM*/
-	LCD_voidSendCommand(128);
+	LCD_voidSendCommand(CGRAM_TO_DDRAM);
 	LCD_voidSetPosition(Copy_u8Row, Copy_u8Col);
 
 	/*First eight character which saved at CGRAM*/
-	for (Local_u8Iteration2 = 0; Local_u8Iteration2 <= 7; Local_u8Iteration2++){
-
+	for (Local_u8Iteration2 = 0; Local_u8Iteration2 <= 7; Local_u8Iteration2++)
+	{
 		/* Write bytes of DDRAM */
 		LCD_voidWriteData(Local_u8Iteration2);
 		_delay_ms(5);
@@ -329,22 +286,78 @@ void LCD_voidWriteExtraChar(uint8 Copy_u8Row, uint8 Copy_u8Col){
 
 
 /******************************************************************************
+************************ Private Functions Implementation *********************
+******************************************************************************/
+
+/******************************************************************************
+* Description 	: LCD Send Command Function.						          *
+******************************************************************************/
+
+static void LCD_voidSendCommand(uint8 Copy_u8Command)
+{
+	DIO_voidSetPinValue (LCD_RS_PIN, DIO_LOW);
+	DIO_voidSetPinValue (LCD_RW_PIN, DIO_LOW);
+
+	#if (LCD_MODE == MODE_8_BIT)
+
+		DIO_voidSetPinValue(LCD_D0_PIN, GET_BIT(Copy_u8Command, 0));
+		DIO_voidSetPinValue(LCD_D1_PIN, GET_BIT(Copy_u8Command, 1));
+		DIO_voidSetPinValue(LCD_D2_PIN, GET_BIT(Copy_u8Command, 2));
+		DIO_voidSetPinValue(LCD_D3_PIN, GET_BIT(Copy_u8Command, 3));
+		DIO_voidSetPinValue(LCD_D4_PIN, GET_BIT(Copy_u8Command, 4));
+		DIO_voidSetPinValue(LCD_D5_PIN, GET_BIT(Copy_u8Command, 5));
+		DIO_voidSetPinValue(LCD_D6_PIN, GET_BIT(Copy_u8Command, 6));
+		DIO_voidSetPinValue(LCD_D7_PIN, GET_BIT(Copy_u8Command, 7));
+
+		LCD_voidLatchEnable();
+
+	#elif (LCD_MODE == MODE_4_BIT)
+
+		DIO_voidSetPinValue(LCD_D4_PIN, GET_BIT(Copy_u8Command, 4));
+		DIO_voidSetPinValue(LCD_D5_PIN, GET_BIT(Copy_u8Command, 5));
+		DIO_voidSetPinValue(LCD_D6_PIN, GET_BIT(Copy_u8Command, 6));
+		DIO_voidSetPinValue(LCD_D7_PIN, GET_BIT(Copy_u8Command, 7));
+
+		LCD_voidLatchEnable();
+
+		DIO_voidSetPinValue(LCD_D4_PIN, GET_BIT(Copy_u8Command, 0));
+		DIO_voidSetPinValue(LCD_D5_PIN, GET_BIT(Copy_u8Command, 1));
+		DIO_voidSetPinValue(LCD_D6_PIN, GET_BIT(Copy_u8Command, 2));
+		DIO_voidSetPinValue(LCD_D7_PIN, GET_BIT(Copy_u8Command, 3));
+
+		LCD_voidLatchEnable();
+
+	#endif
+
+}
+
+/******************************************************************************
 * Description 	: Get Power Private Function.					              *
 ******************************************************************************/
 
-static uint16 Private_u16GetPower(uint8 Copy_u8Number1, uint8 Copy_u8Number2){
-
+static uint16 Private_u16GetPower(uint8 Copy_u8Number1, uint8 Copy_u8Number2)
+{
 	uint16 Local_u16Result = 1;
 	uint8 Local_u8Counter = 0;
 
-	for(Local_u8Counter = 0; Local_u8Counter < Copy_u8Number2; Local_u8Counter++){
-
+	for(Local_u8Counter = 0; Local_u8Counter < Copy_u8Number2; Local_u8Counter++)
+	{
 		Local_u16Result *= Copy_u8Number1;
 	}
 	return Local_u16Result;
 }
 
+/******************************************************************************
+* Description 	: Function to send High to Low enable pulse to lcd.			  *
+******************************************************************************/
 
+static void LCD_voidLatchEnable(void)
+{
+	DIO_voidSetPinValue(LCD_EN_PIN, DIO_HIGH);
+	_delay_ms(1);
+	DIO_voidSetPinValue(LCD_EN_PIN, DIO_LOW);
+	_delay_ms(1);
+}
 
 /******************************************************************************
 ********************************* END OF PROGRAM ******************************
